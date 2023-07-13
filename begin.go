d902a20d3a19d3152a177645622242efa5cd33b0
Markelov Aleksey <aleksey.markelov@vk.team>
2023-07-13 19:25:01 +0300

    Методы для работы с пользователем

--- begin.go
+++ begin.go
@@ -2,16 +2,34 @@ package begin
 
 import "github.com/google/uuid"
 
+const accessAge uint = 18
+
+func New(name string, age uint) *Profile {
+	return &Profile{
+		Uuid: uuid.New(),
+		Name: name,
+		Age:  age,
+	}
+}
+
 type Profile struct {
 	Uuid uuid.UUID
 	Name string
 	Age  uint
 }
 
-func (profile *Profile) New(name string, age uint) *Profile {
-	return &Profile{
-		Uuid: uuid.New(),
-		Name: name,
-		Age:  age,
-	}
+func (p Profile) GetName() string {
+	return p.Name
+}
+
+func (p Profile) GetUuid() string {
+	return p.Uuid.String()
+}
+
+func (p Profile) GetAge() uint {
+	return p.Age
+}
+
+func (p Profile) AcceptAge() bool {
+	return p.GetAge() > accessAge
 }
